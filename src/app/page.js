737fada7d795310584ca4b1dc0e08f87d8320371
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-03-29 15:38:53 +0700

    install next-ui and framer motion

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,18 @@
+import { Button } from "@nextui-org/react";
+import { Badge, Avatar } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <main>Hello</main>
+    <main className="m-5">
+      <Button color="success">Success</Button>
+
+      <Badge content="5" color="primary">
+        <Avatar
+          radius="md"
+          size="lg"
+          src="https://i.pravatar.cc/300?u=a042581f4e29026709d"
+        />
+      </Badge>
+    </main>
   );
 }
