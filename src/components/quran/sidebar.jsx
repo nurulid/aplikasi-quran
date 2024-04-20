dda3454e4fe4ec37961c19bfce1c73d4918ef689
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-04-21 00:10:45 +0700

    temporary fix

--- src/components/quran/sidebar.jsx
+++ src/components/quran/sidebar.jsx
@@ -19,6 +19,13 @@ export async function Sidebar() {
       >
         <SquareMenu size={30} className="" />
       </span>
+      <aside className="p-5 lg:pb-10 border border-dashed rounded  left-0 bottom-16 lg:sticky z-[10] lg:top-0 w-full lg:max-w-[360px] h-[88vh] overflow-hidden hidden lg:block bg-lemonade">
+        <SurahList
+          titleOnly={true}
+          listDisplay={'space-y-3'}
+          listHeight={'h-[calc(100vh-200px)] overflow-scroll pr-2 pb-8'}
+        />
+      </aside>
       {isOpen && (
         <aside className="p-5 lg:pb-10 border border-dashed rounded absolute left-0 bottom-16 lg:sticky z-[10] lg:top-0 w-full lg:max-w-[360px] h-[88vh] overflow-hidden block bg-lemonade">
           <SurahList
