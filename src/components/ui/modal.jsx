f166b6c60962b45005146d71b8cbc5a014582b18
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-05-09 23:16:25 +0700

    improve tafsir modal

--- src/components/ui/modal.jsx
+++ src/components/ui/modal.jsx
@@ -15,7 +15,7 @@ export const Modal = ({
           onClick={closeModal}
           className="bg-black/30 backdrop-blur absolute inset-0"
         ></div>
-        <div className="modal-box max-w-3xl p-6 md:p-10 pb-0">
+        <div className="modal-box max-w-3xl p-6 md:p-10">
           <form method="dialog">
             <button
               className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 cursor-pointer"
@@ -28,7 +28,7 @@ export const Modal = ({
           <div>{modalHeader}</div>
           {innerHTML ? (
           <div
-            className="py-4"
+            className="py-4 border-y mt-4 border-dashed border-primary/20"
             dangerouslySetInnerHTML={{
               __html: content,
             }}
