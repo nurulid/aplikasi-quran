d8e750c224ec0f228dd03501df4118ebf7dac482
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-07-21 21:03:05 +0700

    feat: add active state to theme switcher

--- src/components/ui/themeSwitcher.jsx
+++ src/components/ui/themeSwitcher.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useTheme } from '@/hooks/useTheme';
 
 const themeColors = ['lemonade', 'forest', 'dim', 'lofi'];
@@ -8,20 +8,24 @@ const arabFonts = [
   { name: 'Noto Sans Arabic', value: 'font-arabic' },
   { name: 'IBM Plex Sans Arabic', value: 'fontIbm' },
   { name: 'Noto Kufi Arabic', value: 'fontKufi' },
-]
+];
 
 export const ThemeSwitcher = () => {
   const { changeTheme, changeFont } = useTheme();
+  const [activeTheme, setActiveTheme] = useState('lemonade');
+  const [activeFont, setActiveFont] = useState('font-arabic');
 
   const toggleTheme = (theme) => {
     changeTheme(theme);
     document.documentElement.setAttribute('data-theme', theme);
     localStorage.setItem('theme', theme);
+    setActiveTheme(theme);
   };
 
   const toggleFont = (font) => {
     changeFont(font);
     localStorage.setItem('font', font);
+    setActiveFont(font);
   };
 
   useEffect(() => {
@@ -30,10 +34,12 @@ export const ThemeSwitcher = () => {
 
     if (themeActive) {
       toggleTheme(themeActive);
+      setActiveFont(themeActive);
     }
 
     if (fontActive) {
-      toggleFont(fontActive)
+      toggleFont(fontActive);
+      setActiveFont(fontActive);
     }
   }, []);
 
@@ -43,7 +49,10 @@ export const ThemeSwitcher = () => {
       <div className="flex gap-3 mb-8">
         {themeColors.map((theme) => (
           <button
-            className="p-2 border hover:opacity-85 transition-all rounded capitalize"
+            className={[
+              'p-2 border hover:opacity-85 transition-all rounded capitalize',
+              activeTheme === theme ? 'border-green-400' : '',
+            ].join(' ')}
             key={theme}
             onClick={() => toggleTheme(theme)}
             data-theme={theme}
@@ -57,7 +66,10 @@ export const ThemeSwitcher = () => {
       <div className="flex flex-col gap-2 mb-8">
         {arabFonts.map(({ name, value }, i) => (
           <button
-            className="p-2 border hover:opacity-85 transition-all rounded capitalize"
+            className={[
+              'p-2 border hover:opacity-85 transition-all rounded capitalize',
+              activeFont === value ? 'border-green-400' : '',
+            ].join(' ')}
             key={i}
             onClick={() => toggleFont(value)}
           >
